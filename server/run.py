0f411cdfcf7a04a530c693a18c058f271a5b3145
chenxuetian <chenxuetianofficial@gmail.com>
2023-12-12 23:14:30 +0800

    Modify the server ip

--- server/run.py
+++ server/run.py
@@ -30,4 +30,4 @@ def _get_example_data():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5400, use_reloader=True, debug=True)
+    app.run(host='10.222.148.31', port=5400, use_reloader=True, debug=True)
