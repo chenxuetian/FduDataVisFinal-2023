1c3876228a50c7f8c7c6579d51c7acb4dda4d1a8
nlx0021 <nlxliwenye@126.com>
2023-12-20 15:05:26 +0800

    framework of idfig

--- static/js/idfig.js
+++ static/js/idfig.js
@@ -8,6 +8,7 @@ function IdFig(pos, size) {
   this.innerWidth = size.width - this.margin.left - this.margin.right;
   this.innerHeight = size.height - this.margin.top - this.margin.bottom;
 
+  // 个人页面主svg
   this.fig = d3
     .select("#mainsvg")
     .append("svg")
@@ -16,4 +17,26 @@ function IdFig(pos, size) {
     .attr("y", this.y)
     .attr("width", this.outerWidth)
     .attr("height", this.outerHeight);
+
+  // id选择图svg
+  this.idsvg = this.fig.append("svg").attr("id", "idsvg");
+  // 雷达图svg
+  this.radiofig = this.fig.append("svg").attr("id", "radiofig");
+  // .attr("x", ...) and so on
+  // 直方图svg
+  this.fig.append("svg").attr("id", "histfig").attr("id", "histfig");
+  // .attr("X", ...) and so on
 }
+
+IdFig.prototype.show = function (allData) {
+  // 请在这里绘制各个函数
+  function renderId() {}
+  function renderRadio() {}
+  function renderHist() {}
+
+  renderId();
+  renderRadio();
+  renderHist();
+};
+
+IdFig.prototype.updateId = function (Id) {};
